9345fb5361f3e4be4df2737940e614e430e58f0a
jisooooooooooo <dlawltn0708@gmail.com>
2023-11-13 00:29:54 +0900

    헤더 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
 import React from "react";
+import "../styles/Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header-body">
+      <div className="header-title">MURO</div>
+      <div className="header-menu">
+        <div>장례서비스</div>
+        <div>마이페이지</div>
+        <button className="loginbtn">로그아웃</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
