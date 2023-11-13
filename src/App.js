848b76f9eb66e467d21950ac0e99e6220572af2b
jisooooooooooo <dlawltn0708@gmail.com>
2023-11-13 11:44:33 +0900

    body 메뉴 라우터

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import FuneralService from "./pages/FuneralService";
-import "./App.css"
+import "./App.css";
 import Mypage from "./pages/Mypage";
+import Search from "./pages/Search";
+import Body from "./components/Body";
+import Instructor from "./pages/Instructor";
+import Guide from "./pages/Guide";
+import Review from "./pages/Review";
 
 function App() {
   return (
@@ -13,8 +18,15 @@ function App() {
         <div className="img"></div>
         <Routes>
           <Route path="/funeral-service" element={<FuneralService />} />
-          <Route path="/mypage" element={<Mypage />}/>
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/instructor" element={<Instructor />} />
+          <Route path="/guide" element={<Guide />} />
+          <Route path="/review" element={<Review />} />
         </Routes>
+        <Body />
+        
+
       </div>
     </Router>
   );
