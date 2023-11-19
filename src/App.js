ea39c0a66285f3d9c5dd20013304e6f995ec896f
jisooooooooooo <dlawltn0708@gmail.com>
2023-11-19 16:20:13 +0900

    메인화면 문구 추가

--- src/App.js
+++ src/App.js
@@ -22,6 +22,21 @@ function App() {
               <>
                 <div className="img"></div>
                 <Body />
+                <div className="intro">
+                  무로, 당신의 가족이 되어주었던 소중한 반려동물에게
+                  <br />
+                  마지막까지 정성을 다하는 장례 서비스 입니다.
+                </div>
+                <div className="intro">
+                  '무로'는 무지개로드의 줄임말로
+                  <br />
+                  마지막 길을 걷는 추억하자는 의미를 담고 있습니다.
+                </div>
+                <div className="intro">
+                  무로는 그동안 함께한 소중한 동행자에게
+                  <br />
+                  마지막까지 정성스러운 이별을 선사하기 위해 노력하고 있습니다.
+                </div>
               </>
             }
           />
