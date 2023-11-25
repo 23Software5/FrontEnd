ed2a3f7c592c3e7d59562b2b2d91cc00fea92a73
jisooooooooooo <dlawltn0708@gmail.com>
2023-11-25 14:04:47 +0900

    Fix: ReviewBoard, FuneralDetailList error

--- src/pages/ReviewBoard.js
+++ src/pages/ReviewBoard.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "../styles/ReviewBoard.css";
 
-const ReviewBoard = ({ selectedFuneralHome }) => {
+const ReviewBoard = ({ selectedFuneralHome, hideBannerAndSearchBar  }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   const [reviews, setReviews] = useState([
     {
       name: "김OO",
@@ -26,28 +28,54 @@ const ReviewBoard = ({ selectedFuneralHome }) => {
 
   useEffect(() => {
     const filtered = selectedFuneralHome
-      ? reviews.filter((review) => review.funeralHome === selectedFuneralHome.name)
+      ? reviews.filter(
+          (review) => review.funeralHome === selectedFuneralHome.name
+        )
       : reviews;
 
     setFilteredReviews(filtered);
   }, [selectedFuneralHome, reviews]);
 
+  const handleSearch = () => {
+    const filtered = reviews.filter((review) =>
+      review.funeralHome.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredReviews(filtered);
+  };
   return (
-    <div className="review-list">
-      
-      <div className="review-list-items">
-        {filteredReviews.map((review, index) => (
-          <div key={index} className="review-box">
-            <div className="review-header">
-              <div>
-                <span className="name">{review.name}</span> |{" "}
-                <span className="date">{review.date}</span>
-              </div>
-              <div className="funeral-home">{review.funeralHome}</div>
-            </div>
-            <div className="content">{review.content}</div>
+    <div>
+      {!hideBannerAndSearchBar && (
+        <>
+          <div className="review-list-banner">후기 게시판</div>
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="검색할 장례식장을 입력하세요"
+              value={searchTerm}
+              className="reviewboard-search"
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button className="reviewboard-search-btn" onClick={handleSearch}>
+              검색
+            </button>
           </div>
-        ))}
+        </>
+      )}
+      <div className="review-list">
+        <div className="review-list-items">
+          {filteredReviews.map((review, index) => (
+            <div key={index} className="review-box">
+              <div className="review-header">
+                <div>
+                  <span className="name">{review.name}</span> |{" "}
+                  <span className="date">{review.date}</span>
+                </div>
+                <div className="funeral-home">{review.funeralHome}</div>
+              </div>
+              <div className="content">{review.content}</div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
