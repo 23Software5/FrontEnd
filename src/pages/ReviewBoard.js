1a1870047e8a7d284c378c3d758b7b1f56b46e45
jisooooooooooo <dlawltn0708@gmail.com>
2023-11-26 01:12:30 +0900

    Docs

--- src/pages/ReviewBoard.js
+++ src/pages/ReviewBoard.js
@@ -11,7 +11,6 @@ const ReviewBoard = ({ selectedFuneralHome, hideBannerAndSearchBar  }) => {
       funeralHome: "ABC 장례식장",
       content: "서비스가 훌륭합니다!",
     },
-    
     {
       name: "이OO",
       date: "2023-01-01",
