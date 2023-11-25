9b8ff152c112e72d3b834cc025680d3d0fce336d
kitechoi <chevel0212@gmail.com>
2023-11-25 16:21:30 +0900

    feature2

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import "../styles/Mypage.css";
 import { Link } from 'react-router-dom';
 
 const Mypage = () => {
   return (
     <div>
-      <h1>Mypage</h1>
-      <Link to="/myreview">
-        <button>내가 쓴 글</button>
-      </Link>
-      <Link to="/mysearch">
-        <button>내 견적 확인</button>
-      </Link>
+      <div className='my-page-top'>마이페이지</div>
+      <div className='my-management'>회원정보관리</div>
+
+      <div className='container'>
+        
+        <br/>
+
+        <div>
+          <Link to="/myreview">
+            <button>내가 쓴 글</button>
+          </Link>
+          <Link to="/mysearch">
+            <button>내 견적 확인</button>
+          </Link>
+        </div>
+
+      </div>
     </div>
   );
 }
