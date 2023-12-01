d144fbc2b4817cf99346018b87cf08557cdf727b
jisooooooooooo <dlawltn0708@gmail.com>
2023-12-01 13:55:14 +0900

    Fix: MySearch page modal fix

--- src/pages/MyReview.js
+++ src/pages/MyReview.js
@@ -1,9 +1,6 @@
-// MyReview.js
 import React, { useState, useEffect } from "react";
 import "../styles/MyReview.css";
-import axios from "axios"; // axios import 추가
-
-const baseUrl = "http://localhost:8080"; // 서버의 주소에 맞게 수정
+import { getReviewById } from "../Api";
 
 const MyReview = () => {
   const [userReviews, setUserReviews] = useState([]);
@@ -11,44 +8,24 @@ const MyReview = () => {
   useEffect(() => {
     const fetchUserReviews = async () => {
       try {
-        const response = await axios.get(`${baseUrl}/reviews`);
-        const reviews = response.data;
-        setUserReviews(reviews);
+        const userId = localStorage.getItem("userId");
+        const reviews = await getReviewById(userId);
+        setUserReviews(reviews || []);
       } catch (error) {
         console.error("Error fetching user reviews:", error);
-
-        // 서버에서 데이터를 가져오지 못할 때만 더미 데이터 사용
-        setUserReviews([
-          {
-            name: "김OO",
-            date: "2023-11-14",
-            funeralHome: "ABC 장례식장",
-            content: "서비스가 훌륭합니다!",
-          },
-        ]);
       }
     };
 
     fetchUserReviews();
   }, []);
 
-  const handleDeleteReview = async (index) => {
-    try {
-      const reviewId = userReviews[index].id;
-      const response = await axios.delete(`${baseUrl}/reviews/${reviewId}`);
-      const updatedReviews = [...userReviews];
-      updatedReviews.splice(index, 1);
-      setUserReviews(updatedReviews);
-    } catch (error) {
-      console.error("Error deleting review:", error);
-    }
-  };
-
   return (
     <div>
       <div className="img">
         <div className="Search-imgcontent">
-          <span style={{ fontSize: '40px', fontWeight: 'bold' }}>내가 쓴 글 확인하기</span>
+          <span style={{ fontSize: "40px", fontWeight: "bold" }}>
+            내가 쓴 글 확인하기
+          </span>
         </div>
       </div>
       <div className="my-review-middle">내 후기 조회</div>
@@ -57,18 +34,13 @@ const MyReview = () => {
           <div key={index} className="review-box">
             <div className="review-header">
               <div>
-                <span className="name">{review.name}</span> |{" "}
-                <span className="date">{review.date}</span>
+                <span className="name">{review.user?.nickname}</span> |{" "}
+                <span className="date">{review.fh_date}</span>
               </div>
-              <div className="funeral-home">{review.funeralHome}</div>
+              <div className="funeral-home">{review.funeralhall?.fh_name}</div>
             </div>
-            <div className="content">{review.content}</div>
-            <button
-              className="delete-button"
-              onClick={() => handleDeleteReview(index)}
-            >
-              삭제하기
-            </button>
+            <div className="content">{review.reviewText}</div>
+            <button className="delete-button">삭제하기</button>
           </div>
         ))}
       </div>
--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -4,79 +4,21 @@ import "../styles/Mypage.css";
 import { Link } from "react-router-dom";
 import * as api from "../Api";
 
+
 const Mypage = () => {
   const [user, setUser] = useState(null);
-  const [isEditing, setIsEditing] = useState(false);
-  const [editedName, setEditedName] = useState("");
-  const [editedPhoneNumber, setEditedPhoneNumber] = useState("");
 
   useEffect(() => {
     const userId = localStorage.getItem("userId");
-    console.log("userId:", userId);
 
-    api
-      .getUserProfile(userId)
+    api.getUserProfile(userId)
       .then((userData) => {
         setUser(userData);
       })
       .catch((error) => {
-        console.error("Error fetching user data:", error);
-        setUser(dummyData);
+        console.error("사용자 정보 가져오기 오류:", error);
       });
   }, []);
-
-  const handleEdit = () => {
-    setEditedName(user.name);
-    setEditedPhoneNumber(user.phoneNumber);
-    setIsEditing(true);
-  };
-
-  const handleSave = () => {
-    const trimmedName = editedName.trim();
-    const trimmedPhoneNumber = editedPhoneNumber.trim();
-  
-    if (trimmedName !== "" && trimmedPhoneNumber !== "") {
-      // localStorage에서 userId 가져오기
-      const userId = localStorage.getItem("userId");
-  
-      // 사용자 데이터를 업데이트하는 API 요청
-      axios
-        .put(`/users/${userId}`, {
-          email: user.email,
-          password: user.password,
-          nickname: trimmedName,
-          phonenumber: trimmedPhoneNumber,
-        })
-        .then(() => {
-          setUser({
-            ...user,
-            name: trimmedName,
-            phoneNumber: trimmedPhoneNumber,
-          });
-          setIsEditing(false);
-        })
-        .catch((error) => {
-          console.error("사용자 데이터 업데이트 오류:", error);
-          window.alert("사용자 데이터 업데이트에 실패했습니다.");
-        });
-    } else {
-      window.alert("이름과 전화번호는 비워둘 수 없습니다.");
-    }
-  };
-  
-  
-
-  const handleCancel = () => {
-    setIsEditing(false);
-  };
-
-  const handleChangeName = (e) => {
-    setEditedName(e.target.value);
-  };
-
-  const handleChangePhoneNumber = (e) => {
-    setEditedPhoneNumber(e.target.value);
-  };
 
   if (!user) {
     return <div>Loading...</div>;
@@ -93,17 +35,7 @@ const Mypage = () => {
             <tbody>
               <tr>
                 <th>이름</th>
-                <td>
-                  {isEditing ? (
-                    <input
-                      type="text"
-                      value={editedName}
-                      onChange={handleChangeName}
-                    />
-                  ) : (
-                    user.name
-                  )}
-                </td>
+                <td>{user.nickname}</td>
               </tr>
               <tr>
                 <th>이메일</th>
@@ -115,17 +47,7 @@ const Mypage = () => {
               </tr>
               <tr>
                 <th>전화번호</th>
-                <td>
-                  {isEditing ? (
-                    <input
-                      type="text"
-                      value={editedPhoneNumber}
-                      onChange={handleChangePhoneNumber}
-                    />
-                  ) : (
-                    user.phoneNumber
-                  )}
-                </td>
+                <td>{user.phoneNumber}</td>
               </tr>
             </tbody>
           </table>
@@ -133,21 +55,6 @@ const Mypage = () => {
         <br />
 
         <div>
-          {isEditing ? (
-            <>
-              <button className="my-save-btn" onClick={handleSave}>
-                <div className="my-update-btn-name">저장</div>
-              </button>
-              <button className="my-cancel-btn" onClick={handleCancel}>
-                <div className="my-update-btn-name">취소</div>
-              </button>
-            </>
-          ) : (
-            <button className="my-update-btn" onClick={handleEdit}>
-              <div className="my-update-btn-name">정보수정</div>
-            </button>
-          )}
-
           <button className="my-withdraw-btn">
             <div className="my-withdraw-btn-name">탈퇴하기</div>
           </button>
